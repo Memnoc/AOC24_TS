9436c5741be54450d65c2eb76be9e6bf32de6120
Memnoc <smartdroidesign@gmail.com>
2024-12-01 09:42:04 +0000

    added a template to show the day solution in style

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { part1, part2 } from "./day1/solution";
+// src/index.ts
 import { readInput } from "./utils/readIinput";
 
-const day = process.argv[2] || "1";
-const input = readInput(parseInt(day));
+async function runDay(day: string) {
+  try {
+    const { part1, part2 } = await import(`./day${day}/solution.ts`);
+    const input = readInput(parseInt(day));
 
-console.log(`Day ${day} Results: `);
-console.log("Part 1: ", part1(input));
-console.log("Part 2: ", part2(input));
+    console.log(`\n🎄 Day ${day} Solutions:`);
+    console.log("Part 1:", part1(input));
+    console.log("Part 2:", part2(input));
+  } catch (e) {
+    console.error(`Failed to run day ${day}:`, e);
+  }
+}
+
+const day = process.argv[2] || "1";
+runDay(day);
