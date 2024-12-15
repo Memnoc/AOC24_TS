0db7fce63d480c7b74a4aacf6c81c32fe688a873
Memnoc <smartdroidesign@gmail.com>
2024-12-15 20:20:12 +0000

    test for day 2 part one and git ignore

--- src/day2/solution.test.ts
+++ src/day2/solution.test.ts
@@ -1,16 +1,19 @@
-import { expect, test, describe } from 'vitest';
-import { part1, part2 } from './solution';
+import { expect, test, describe } from "vitest";
+import { part1, part2 } from "./solution";
 
-const SAMPLE_INPUT = `sample
-input
-here`;
+const SAMPLE_INPUT = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`;
 
-describe('Day 2', () => {
-  test('part 1', () => {
-    expect(part1(SAMPLE_INPUT)).toBe(0);
+describe("Day 2", () => {
+  test("part 1", () => {
+    expect(part1(SAMPLE_INPUT)).toBe(2);
   });
 
-  test('part 2', () => {
+  test("part 2", () => {
     expect(part2(SAMPLE_INPUT)).toBe(0);
   });
 });
